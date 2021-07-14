f783ef84718d7f0fc5e1e55f874de5fb0362f0d3
marc0olo <walz.marcoo@gmail.com>
2021-07-14 05:58:11 +0200

    chore(aepp-sdk-js): v8.2.1

--- governance_aepp_delegate_voting_power.js
+++ governance_aepp_delegate_voting_power.js
@@ -1,4 +1,4 @@
-const { Universal: Ae, MemoryAccount, Node, Crypto } = require('@aeternity/aepp-sdk')
+const { Universal, MemoryAccount, Node, Crypto } = require('@aeternity/aepp-sdk')
 const fs = require('fs')
 
 const CONTRACT_SOURCE = fs.readFileSync('./contracts/GovernanceRegistryInterface.aes', 'utf8')
@@ -25,31 +25,29 @@ const SETTINGS = {
     TESTNET: {
         nodeUrl: 'https://testnet.aeternity.io',
         compilerUrl: 'https://compiler.aepps.com',
-        middlewareUrl: 'https://testnet.aeternity.io/mdw/',
+        middlewareUrl: 'https://testnet.aeternity.io/mdw',
         contractAddress: 'ct_2nritSnqW6zooEL4g2SMW5pf12GUbrNyZ17osTLrap7wXiSSjf',
       },
     MAINNET: {
         nodeUrl: 'https://mainnet.aeternity.io',
         compilerUrl: 'https://compiler.aepps.com',
-        middlewareUrl: 'https://mainnet.aeternity.io/mdw/',
+        middlewareUrl: 'https://mainnet.aeternity.io/mdw',
         contractAddress: 'ct_ouZib4wT9cNwgRA1pxgA63XEUd8eQRrG8PcePDEYogBc1VYTq',
     }
 }
 
 const main = async () => {
-    const node = await Node({ url: SETTINGS[AE_NETWORK].nodeUrl, internalUrl: SETTINGS[AE_NETWORK].nodeUrl })
-    const account = MemoryAccount({ keypair: KEYPAIR })
-    const AeClient = await Ae({
+    const node = await Node({ url: SETTINGS[AE_NETWORK].nodeUrl })
+    const client = await Universal({
         nodes: [
           { name: AE_NETWORK, instance: node },
         ],
         compilerUrl: SETTINGS[AE_NETWORK].compilerUrl,
-        accounts: [account],
-        address: KEYPAIR.publicKey
+        accounts: [MemoryAccount({ keypair: KEYPAIR })],
     })
-    const contractObject = await AeClient.getContractInstance(CONTRACT_SOURCE, { contractAddress: SETTINGS[AE_NETWORK].contractAddress })
-    callResult = await contractObject.methods.delegate(DELEGATEE)
-    console.log(callResult)
+    const contractInstance = await client.getContractInstance(CONTRACT_SOURCE, { contractAddress: SETTINGS[AE_NETWORK].contractAddress })
+    const dryRunCall = await contractInstance.methods.delegate(DELEGATEE)
+    console.log(dryRunCall)
 }
 
 main()
